ccd4a47188d9aca29f3ce46af42be622a06a33c5
aisyah <nuraisyah1004@gmail.com>
2023-06-09 02:33:27 +0800

    edited

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'FSKTM HUB',
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+      ),
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fsktm_hub/reusable_widgets/reusable_widgets.dart';
 import 'package:fsktm_hub/utils/color_utils.dart';
 
 class Dashboard extends StatefulWidget {
@@ -14,11 +15,28 @@ class _DashboardState extends State<Dashboard> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Text("FSKTM HUB"),
-        flexibleSpace: Container(
-          height: 250,
-          width: MediaQuery.of(context).size.width,
-          color: hexStringToColor("#A478B8"),
+        toolbarHeight: 250,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        flexibleSpace: ClipPath(
+          clipper: CustomShape(),
+          child: Container(
+            height: 300,
+            width: MediaQuery.of(context).size.width,
+            color: hexStringToColor("#A478B8"),
+            child: Container(
+              padding: EdgeInsets.fromLTRB(80, 80, 0, 0),
+              child: Text(
+                "FSKTM Hub",
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w700,
+                  fontSize: 40,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
         ),
       ),
       body: SingleChildScrollView(
