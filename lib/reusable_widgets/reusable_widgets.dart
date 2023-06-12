a03dd63b6ad87aada7d6c4a080d0fc33496c209e
lavydel <u2005322@siswa.um.edu.my>
2023-06-13 04:36:40 +0800

    donde

--- lib/reusable_widgets/reusable_widgets.dart
+++ lib/reusable_widgets/reusable_widgets.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:fsktm_hub/screens/blockb_screen.dart';
+
+import '../screens/blocka_screen.dart';
 
 Image logoWidget(String imageName) {
   return Image.asset(
@@ -31,7 +35,7 @@ Image blockPicture(String imageName) {
   return Image.asset(
     imageName,
     fit: BoxFit.fitWidth,
-    width: 300,
+    width: 600,
     height: 300,
   );
 }
@@ -54,4 +58,80 @@ class CustomShape extends CustomClipper<Path> {
   bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
     return true;
   }
+}
+
+Material displayimage1(BuildContext context) {
+  return Material(
+    elevation: 10,
+    borderRadius: BorderRadius.circular(30),
+    clipBehavior: Clip.antiAliasWithSaveLayer,
+    child: InkWell(
+      splashColor: Colors.black26,
+      onTap: () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const blockA(),
+          ),
+        );
+      },
+      child: Ink.image(
+        image: NetworkImage(
+            'https://fsktm.um.edu.my/fsktm/images/HOME/fsktm-front.png'),
+        height: 200,
+        colorFilter:
+            ColorFilter.mode(Colors.black.withOpacity(0.25), BlendMode.srcOver),
+        fit: BoxFit.cover,
+        child: Center(
+          child: Text(
+            'Block A',
+            style: TextStyle(
+              fontSize: 40,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+Material displayimage2(BuildContext context) {
+  return Material(
+    elevation: 10,
+    borderRadius: BorderRadius.circular(30),
+    clipBehavior: Clip.antiAliasWithSaveLayer,
+    child: InkWell(
+      splashColor: Colors.black26,
+      onTap: () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const blockB(),
+          ),
+        );
+      },
+      child: Ink.image(
+        image: NetworkImage(
+            'https://fsktm.um.edu.my/fsktm/images/HOME/fsktm-front.png'),
+        height: 200,
+        colorFilter:
+            ColorFilter.mode(Colors.black.withOpacity(0.25), BlendMode.srcOver),
+        fit: BoxFit.cover,
+        child: Center(
+          child: Text(
+            'Block B',
+            style: TextStyle(
+              fontSize: 40,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    ),
+  );
 }
